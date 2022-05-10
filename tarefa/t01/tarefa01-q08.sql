379658a924c9354fabc59387db39fe603b3df61f
Rauan Meirelles <77637967+rauan-meirelles@users.noreply.github.com>
2022-05-10 10:11:19 -0300

    Criando o VIEW questão 08 #1

---
+++ tarefa/t01/tarefa01-q08.sql
@@ -0,0 +1,8 @@
+#Faça uma consulta que selecione o nome do departamento, o nome do gerente, e o número de projetos de cada departamento. Deve aparecer os departamentos sem gerente e sem projetos. Crie e use views na consulta, se necessário.
+
+CREATE VIEW vwProjetos(gerente, departamento, quant_projetos) AS
+SELECT d.cod_gerente, d.descricao, COUNT(p.codigo)
+FROM departamento as d, projeto as p
+WHERE d.codigo = p.cod_depto
+GROUP BY d.descricao, d.cod_gerente;
+
