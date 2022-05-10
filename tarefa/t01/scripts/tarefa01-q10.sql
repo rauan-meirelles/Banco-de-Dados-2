3cc7c84607be3e794762043ddb41aee745868536
Rauan Meirelles <77637967+rauan-meirelles@users.noreply.github.com>
2022-05-10 10:32:40 -0300

    Resolvendo a questão 10 #1

--- tarefa/t01/scripts/tarefa01-q10.sql
+++ tarefa/t01/scripts/tarefa01-q10.sql
@@ -1,2 +1,5 @@
 #Faça uma consulta que selecione o nome do projeto, o nome do departamento do projeto, o nome do funcionário responsável pelo projeto, o nome do departamento do funcionário responsável.
 
+SELECT p.nome, d.descricao, f.nome
+FROM funcionario as f , departamento as d , projeto as p
+WHERE p.cod_depto = d.codigo and p.cod_responsavel = f.codigo
