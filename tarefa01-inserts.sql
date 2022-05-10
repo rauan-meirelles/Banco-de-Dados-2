b4bcc2723fced55381e1b523d59297c27fd4394f
Rauan Meirelles <77637967+rauan-meirelles@users.noreply.github.com>
2022-05-10 09:22:20 -0300

    Inserindo conteúdo na tabela projeto #1

--- tarefa01-inserts.sql
+++ tarefa01-inserts.sql
@@ -1,10 +1,10 @@
 
 INSERT INTO funcionario (nome, sexo, dt_nasc, salario, cod_dep)
-VALUES("Adson Matheus Santos Brito", 'M', '1999-07-09', 2500.00, NULL),
+VALUES("Adson Matheus Santos Brito", 'M', '2000-06-08', 2500.00, NULL),
 ("Allan Gabriel Almeida Oliveira", 'M', '2000-12-31', 3000.00, NULL),
 ("Joan de Azevedo Medeiros", 'M', '2000-12-11', 3500.00, NULL),
 ("Rauan Meirelles Dantas de Araujo", 'M', '2002-05-26', 4000.00, NULL),
-("Wesley Vitor Silva de Morais", 'M', '2001-03-12', 4500.00, NULL),
+("Wesley Vitor Silva de Morais", 'M', '2000-06-15', 4500.00, NULL),
 
 
 INSERT INTO departamento (descricao, cod_gerente)
@@ -20,3 +20,9 @@ UPDATE funcionario SET cod_dep = 3 WHERE codigo = 5;
 UPDATE funcionario SET cod_dep = 4 WHERE codigo = 3;
 UPDATE funcionario SET cod_dep = 5 WHERE codigo = 2;
 
+INSERT INTO projeto(nome, descricao, cod_depto, cod_responsavel, data_inicio, data_fim)
+VALUES("Semana da Educação", "Realização de oficionas e palestras na área de educação", 2, 4, '2022-05-10', '2022-12-31'),
+("Implantação de um sistema de gerenciamento de gastos", "Realizar o gerenciamento dos gastos e dos horários de cada gasto", 1, 1, '2022-05-10', '2022-12-31'),
+("Tecnologias na saúde coletiva", "Se trata de um estudo sobre como a tecnologia pode auxiliar na área da saúde", 4, 3, '2022-05-10', '2022-12-31'),
+("Projeto Esporte e Cidadania", "O projeto busca trazer acesso aos meios de esporte à comunidade", 3, 5, '2022-05-10', '2022-12-31'),
+("A atuação dos assistentes sociais nos CRAS", "O projeto busca monitorar a ação e trabalho dos assitentes sociais nos CRAS's", 5, 2, '2022-05-10', '2022-12-31');
