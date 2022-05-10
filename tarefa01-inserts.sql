3c9342572ba94a856078c586e451288b15db90f1
Rauan Meirelles <77637967+rauan-meirelles@users.noreply.github.com>
2022-05-10 08:51:05 -0300

    Corrigindo conteúdo tabela funcionário

--- tarefa01-inserts.sql
+++ tarefa01-inserts.sql
@@ -4,5 +4,5 @@ VALUES("Adson Matheus Santos Brito", 'M', '1999-07-09', 2500.00, NULL),
 ("Allan Gabriel Almeida Oliveira", 'M', '2000-12-31', 3000.00, NULL),
 ("Joan de Azevedo Medeiros", 'M', '2000-12-31', 3500.00, NULL),
 ("Rauan Meirelles Dantas de Araujo", 'M', '2002-05-26', 4000.00, NULL),
-("Wesley Vitor Silva de Morais", 'M', '2001-03-12', 45000.00, NULL),
+("Wesley Vitor Silva de Morais", 'M', '2001-03-12', 4500.00, NULL),
 
