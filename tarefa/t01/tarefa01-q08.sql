dd90aa5621bef0eb5dda6fbe0efae2c9c060de82
Rauan Meirelles <77637967+rauan-meirelles@users.noreply.github.com>
2022-05-10 10:12:00 -0300

    Implementando a busca tarefa 08 #1

--- tarefa/t01/tarefa01-q08.sql
+++ tarefa/t01/tarefa01-q08.sql
@@ -6,3 +6,7 @@ FROM departamento as d, projeto as p
 WHERE d.codigo = p.cod_depto
 GROUP BY d.descricao, d.cod_gerente;
 
+
+SELECT f.nome, departamento,  quant_projetos
+FROM funcionario as f, vwProjetos as vp
+WHERE f.codigo = vp.gerente
