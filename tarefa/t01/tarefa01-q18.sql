7d54d280121391cbad0717890b3d4b04145de9e7
Rauan Meirelles <77637967+rauan-meirelles@users.noreply.github.com>
2022-05-10 10:22:24 -0300

    Criando tarefa 18 #1

---
+++ tarefa/t01/tarefa01-q18.sql
@@ -0,0 +1,2 @@
+#Faça uma consulta que selecione o nome dos funcionários com maior salário de seu departamento, mesmo que não tenha departamento associado, e o nome dos respectivos departamentos. Exiba em ordem crescente de acordo com o salário.
+
