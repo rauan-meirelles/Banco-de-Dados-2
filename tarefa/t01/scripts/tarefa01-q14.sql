1a8411ea9e82aa6490612cd3028470fa7961b042
Rauan Meirelles <77637967+rauan-meirelles@users.noreply.github.com>
2022-05-10 10:44:48 -0300

    criando VIEW questão 14 #1

--- tarefa/t01/scripts/tarefa01-q14.sql
+++ tarefa/t01/scripts/tarefa01-q14.sql
@@ -1,2 +1,7 @@
 #Faça uma consulta que selecione o nome dos funcionários responsáveis por atividades, o número de atividades que este funcionário é responsável. Crie e use views na consulta.
 
+CREATE VIEW vwAtividade (funcionario, quant) 
+AS SELECT f.nome, count(a.codigo)
+from funcionario as f, atividade as a
+WHERE f.codigo = a.cod_responsavel
+GROUP by f.codigo
