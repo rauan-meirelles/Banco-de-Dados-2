f0e3170ec9184e6c9861bb57213af5f2ec783952
Rauan Meirelles <77637967+rauan-meirelles@users.noreply.github.com>
2022-05-10 10:21:31 -0300

    Criando tarefa 16 #1

---
+++ tarefa/t01/tarefa01-q16.sql
@@ -0,0 +1,2 @@
+#Faça uma consulta que selecione o nome dos responsáveis de projeto que também são gerentes de departamento ou que ganham mais que o gerente de seu departamento. Use views se necessário.
+
