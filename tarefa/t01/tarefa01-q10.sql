98cb67956ac3407c44a75e3610062adad0c9a5e2
Rauan Meirelles <77637967+rauan-meirelles@users.noreply.github.com>
2022-05-10 10:14:22 -0300

    Create tarefa01-q10.sql

---
+++ tarefa/t01/tarefa01-q10.sql
@@ -0,0 +1,2 @@
+#Faça uma consulta que selecione o nome do projeto, o nome do departamento do projeto, o nome do funcionário responsável pelo projeto, o nome do departamento do funcionário responsável.
+
