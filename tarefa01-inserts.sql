9705bf147c435abe92dce2be1591aec4641afe38
Rauan Meirelles <77637967+rauan-meirelles@users.noreply.github.com>
2022-05-10 08:54:52 -0300

    Inserindo conteúdo em tabela departamento #1

--- tarefa01-inserts.sql
+++ tarefa01-inserts.sql
@@ -2,7 +2,15 @@
 INSERT INTO funcionario (nome, sexo, dt_nasc, salario, cod_dep)
 VALUES("Adson Matheus Santos Brito", 'M', '1999-07-09', 2500.00, NULL),
 ("Allan Gabriel Almeida Oliveira", 'M', '2000-12-31', 3000.00, NULL),
-("Joan de Azevedo Medeiros", 'M', '2000-12-31', 3500.00, NULL),
+("Joan de Azevedo Medeiros", 'M', '2000-12-11', 3500.00, NULL),
 ("Rauan Meirelles Dantas de Araujo", 'M', '2002-05-26', 4000.00, NULL),
 ("Wesley Vitor Silva de Morais", 'M', '2001-03-12', 4500.00, NULL),
+
+
+INSERT INTO departamento (descricao, cod_gerente)
+VALUES("Departamento de Finanças", 1),
+("Departamento de Educação", 2),
+("Departamento de Esportes", 3),
+("Departamento de Saúde", 4),
+("Departamento de Assistência Social", 5);
 
