bb61323da0acab6ca64a32ab5be993744ab4ac64
Rauan Meirelles <77637967+rauan-meirelles@users.noreply.github.com>
2022-05-10 08:33:25 -0300

    Criando tabela projeto #1

--- tarefa01-create.sql
+++ tarefa01-create.sql
@@ -23,3 +23,18 @@ CREATE TABLE departamento(
 ALTER TABLE funcionario ADD COLUMN cod_dep INT;
 ALTER TABLE funcionario ADD FOREIGN KEY (cod_dep) REFERENCES departamento(codigo);
 
+
+CREATE TABLE projeto (
+  codigo INT AUTO_INCREMENT, 
+  nome VARCHAR(100) NOT NULL, 
+  descricao VARCHAR(500) NOT NULL, 
+  cod_depto int, 
+  cod_responsavel INT, 
+  data_inicio DATE, 
+  data_fim DATE,
+  
+  PRIMARY KEY(codigo),
+  FOREIGN KEY(cod_depto) REFERENCES departamento(codigo),
+  FOREIGN KEY(cod_responsavel) REFERENCES funcionario(codigo)
+  
+);
