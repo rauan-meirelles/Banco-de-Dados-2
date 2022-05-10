6cab2afad9d74ca90f5b2b2c1e9125b8098308de
Rauan Meirelles <77637967+rauan-meirelles@users.noreply.github.com>
2022-05-10 08:48:29 -0300

    Inserindo conteúdo em tabela funcionário #1

---
+++ tarefa01-inserts.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO funcionario (nome, sexo, dt_nasc, salario, cod_dep)
+VALUES("Adson Matheus Santos Brito", 'M', '1999-07-09', 2500.00, NULL),
+("Allan Gabriel Almeida Oliveira", 'M', '2000-12-31', 3000.00, NULL),
+("Joan de Azevedo Medeiros", 'M', '2000-12-31', 3500.00, NULL),
+("Rauan Meirelles Dantas de Araujo", 'M', '2002-05-26', 4000.00, NULL),
+("Wesley Vitor Silva de Morais", 'M', '2001-03-12', 45000.00, NULL),
+
