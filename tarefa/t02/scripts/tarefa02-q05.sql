6fa3c8a0bec284e07069d5a1c9325f674ec7d57e
Rauan Meirelles <77637967+rauan-meirelles@users.noreply.github.com>
2022-05-21 23:44:19 -0300

    Implementando questão 05 #2

---
+++ tarefa/t02/scripts/tarefa02-q05.sql
@@ -0,0 +1,16 @@
+#Crie um gatilho que ao se adicionar um voo, seja adicionado +1 voo para o piloto daquele voo.
+
+#Trigger
+CREATE TRIGGER addVooToPiloto AFTER 
+INSERT ON voo 
+FOR EACH ROW 
+UPDATE piloto SET num_voos = num_voos + 1 
+WHERE codigo = NEW.cod_piloto; 
+
+# Inserindo piloto
+INSERT INTO piloto(nome, num_voos)
+VALUES ("Alberto Santos Dumont", 3);
+
+# Inserido voo
+INSERT INTO voo(tipo, cod_piloto, num_passageiros, distancia)
+VALUES ("Privado", 1, 150, 2500.00);
