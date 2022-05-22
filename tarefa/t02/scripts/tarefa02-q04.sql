4359147004be1f7422adaba3a076477a9e5aa972
Rauan Meirelles <77637967+rauan-meirelles@users.noreply.github.com>
2022-05-21 23:41:16 -0300

    Implementando tarefa 04 mais inserção #2

--- tarefa/t02/scripts/tarefa02-q04.sql
+++ tarefa/t02/scripts/tarefa02-q04.sql
@@ -1,2 +1,10 @@
 #Crie um gatilho que ao se adicionar um novo cliente, seja criado para ele um cartão de milhagem com 0 milhas.
 
+CREATE TRIGGER addCartaoMilhas AFTER 
+INSERT ON cliente 
+FOR EACH ROW 
+INSERT INTO milhas(cod_cliente, quantidade)
+VALUES (NEW.codigo, 0); 
+
+INSERT INTO cliente(nome, endereco)
+VALUES ("Rauan Meirelles", "Rua Dr Milton Aranha Marinho, Centro, 223, São Fernando/RN");
