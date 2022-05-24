32d0e2285fa2e76214f23c674506d58e9a8d2a37
Rauan Meirelles <77637967+rauan-meirelles@users.noreply.github.com>
2022-05-23 22:06:50 -0300

    Inserindo Cliente, Voo e cliente_voo - questão 07 #2

--- tarefa/t02/scripts/tarefa02-q07.sql
+++ tarefa/t02/scripts/tarefa02-q07.sql
@@ -9,3 +9,15 @@ INSERT ON cliente_voo
 FOR EACH ROW
 UPDATE voo SET num_passageiros = num_passageiros + 1
 WHERE codigo = new.cod_voo;
+
+#Inserindo cliente
+INSERT INTO cliente(nome, endereco)
+VALUES("Taciano Azevedo", "Limoeiro da sapucaí")
+
+#Inserindo um voo
+INSERT INTO voo(tipo, cod_piloto, num_passageiros, distancia)
+VALUES ("Executivo", 1, 1, 1000.00);
+
+#Inserindo um passageiro no voo
+INSERT INTO cliente_voo(cod_cliente, cod_voo, classe)
+VALUES (2, 2, "Executiva");
